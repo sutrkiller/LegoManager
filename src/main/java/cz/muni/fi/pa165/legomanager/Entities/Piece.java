f52251d7954a96896940e9e32558f8877f5f9acf
Tobias <Tobias@147.251.44.79>
2015-10-13 18:22:19 +0200

    first entities

--- src/main/java/cz/muni/fi/pa165/legomanager/Entities/Piece.java
+++ src/main/java/cz/muni/fi/pa165/legomanager/Entities/Piece.java
@@ -5,10 +5,66 @@
  */
 package cz.muni.fi.pa165.legomanager.Entities;
 
+import cz.muni.fi.pa165.legomanager.Enunms.Color;
+import java.util.Objects;
+
 /**
  *
  * @author Tobias
  */
 public class Piece {
+    private Long id;
+    private String name;
+    private Color color;
     
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (! (obj instanceof Piece)) {
+            return false;
+        }
+        final Piece other = (Piece) obj;
+        if (!Objects.equals(this.getName(), other.getName())) {
+            return false;
+        }
+        if (!Objects.equals(this.getColor(), other.getColor())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.getName());
+        hash = 67 * hash + Objects.hashCode(this.getColor());
+        return hash;
+    }
+
 }
