503043bac98f47f9099db6d031b39a488f939707
Sona Mastrakova <sona.mastrakova@gmail.com>
2015-10-17 02:00:28 +0200

    model entity completed It contains: - getter/setter methods for ID, name, price, age limit, category and   pieces - equals and hash methods - toString method

--- src/main/java/cz/muni/fi/pa165/legomanager/Entities/Model.java
+++ src/main/java/cz/muni/fi/pa165/legomanager/Entities/Model.java
@@ -1,14 +1,114 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.muni.fi.pa165.legomanager.Entities;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
 /**
+ * Entity class Model contains name, age limit for children, 
+ * price, category to which the model belongs and 
+ * list of pieces that belong to the model.
  *
- * @author Tobias
+ * @author Sona Mastrakova <sona.mastrakova@gmail.com>
+ * @date 16.10.2015
  */
 public class Model {
-    
+
+    private Long id;
+    private String name;
+    private Byte ageLimit;
+    private BigDecimal price;
+    private Category category;
+    private List<Piece> pieces;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Byte getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(Byte ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<Piece> getPieces() {
+        return pieces;
+    }
+
+    public void setPieces(List<Piece> pieces) {
+        this.pieces = pieces;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + Objects.hashCode(this.ageLimit);
+        hash = 71 * hash + Objects.hashCode(this.price);
+        hash = 71 * hash + Objects.hashCode(this.pieces);
+        hash = 71 * hash + Objects.hashCode(this.category);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (! (obj instanceof Model)) {
+            return false;
+        }
+        final Model other = (Model) obj;
+        if (!Objects.equals(this.name, other.getName())) {
+            return false;
+        }
+        if (!Objects.equals(this.ageLimit, other.getAgeLimit())) {
+            return false;
+        }
+        if (!Objects.equals(this.price, other.getPrice())) {
+            return false;
+        }
+        if (!Objects.equals(this.pieces, other.getPieces())) {
+            return false;
+        }
+        if (!Objects.equals(this.category, other.getCategory())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Model{" + "id=" + id + ", name=" + name + ", price=" + price + ", category=" + category + '}';
+    }
 }
