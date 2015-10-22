876043a18e347145df20bdd66819dc665ff7e472
Laliska <sona.mastrakova@gmail.com>
2015-10-22 22:01:50 +0200

    package name in lowercase

--- src/main/java/cz/muni/fi/pa165/legomanager/entities/Set.java
+++ src/main/java/cz/muni/fi/pa165/legomanager/entities/Set.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.muni.fi.pa165.legomanager.Enums;
+package cz.muni.fi.pa165.legomanager.entities;
 
 /**
  *
  * @author Tobias
  */
-public enum Color {
-    BLACK,WHITE,YELLOW,BLUE,RED,BROWN,GRAY,GREEN,ORANGE,PINK,MAGENTA,CYAN;
+public class Set {
+    
 }
--- src/main/java/cz/muni/fi/pa165/legomanager/enums/Color.java
+++ src/main/java/cz/muni/fi/pa165/legomanager/enums/Color.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cz.muni.fi.pa165.legomanager.Entities;
+package cz.muni.fi.pa165.legomanager.enums;
 
 /**
  *
  * @author Tobias
  */
-public class Category {
-    
+public enum Color {
+    BLACK,WHITE,YELLOW,BLUE,RED,BROWN,GRAY,GREEN,ORANGE,PINK,MAGENTA,CYAN;
 }
