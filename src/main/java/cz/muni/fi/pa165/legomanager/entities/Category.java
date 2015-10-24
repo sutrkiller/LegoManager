af690095dd093ea24567c58ea71dfdf6e101250e
Marek <abaffy.m@gmail.com>
2015-10-24 20:23:17 +0200

    Category class implemented

--- src/main/java/cz/muni/fi/pa165/legomanager/entities/Category.java
+++ src/main/java/cz/muni/fi/pa165/legomanager/entities/Category.java
@@ -5,10 +5,79 @@
  */
 package cz.muni.fi.pa165.legomanager.entities;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
+ * Entity class Category represents a category of Lego model.
+ * It contains name and short description.
  *
- * @author Tobias
+ * @author Marek Abaffy <abaffy.m@gmail.com>
+ * @date 16.10.2015
  */
+@Entity
 public class Category {
-    
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @NotNull
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+
+        Category category = (Category) o;
+
+        if (getName() != null ? !getName().equals(category.getName()) : category.getName() != null) return false;
+        return !(getDescription() != null ? !getDescription().equals(category.getDescription()) : category.getDescription() != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName() != null ? getName().hashCode() : 0;
+        result = 31 * result + (getDescription() != null ? getDescription().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
