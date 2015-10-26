2026134cba279d6a55e95826fd243127338f57ce
ondrejvelisek <ondrejvelisek@gmail.com>
2015-10-26 17:41:40 +0100

    ModelDaoImpl init

---
+++ src/main/java/cz/muni/fi/pa165/legomanager/dao/ModelDaoImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.legomanager.dao;
+
+import cz.muni.fi.pa165.legomanager.entities.Model;
+import java.util.List;
+
+/**
+ *
+ * @author Ondrej Velisek <ondrejvelisek@gmail.com>
+ */
+public class ModelDaoImpl implements ModelDao {
+
+    @Override
+    public void create(Model model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Model findById(Long id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Model findByName(String name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Model> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Model update(Model model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(Model model) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
