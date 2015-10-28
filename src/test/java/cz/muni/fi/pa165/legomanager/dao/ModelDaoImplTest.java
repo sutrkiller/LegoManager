6c84a2b06b5f7684b0a9781fec88be3958e2f3d3
Tobias <tobias.kamenicky@gmail.com>
2015-10-28 14:59:51 +0100

    Recreated test for ModelDao

--- src/test/java/cz/muni/fi/pa165/legomanager/dao/ModelDaoImplTest.java
+++ src/test/java/cz/muni/fi/pa165/legomanager/dao/ModelDaoImplTest.java
@@ -10,11 +10,16 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
 import javax.persistence.PersistenceUnit;
+import javax.validation.ConstraintViolationException;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -28,31 +33,30 @@ import org.testng.annotations.Test;
  * @date 25.10.2015
  */
 @ContextConfiguration(classes = {PersistenceApplicationContext.class})
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
 public class ModelDaoImplTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
     public ModelDao modelDao;
     
+    @Autowired
+    public CategoryDao categoryDao;
+    
     @PersistenceUnit
     private EntityManagerFactory emf;
     
-    //@PersistenceContext
-    private EntityManager e;
-    
     private Model m1;
+    private Model m2;
+    private Model m3;
     private Category c1;
 
     @BeforeMethod
     public void before() {
-        
-        e = emf.createEntityManager();
-        e.getTransaction().begin();
         c1 = new Category();
-        c1.setName("Category-1");
-        c1.setDescription("I have no idea.");
-        e.persist(c1);
-        
-        
+        c1.setDescription("New category");
+        c1.setName("TestingCategory");
+        categoryDao.create(c1);
         
         m1 = new Model();
         m1.setName("Model-1");
@@ -60,15 +64,23 @@ public class ModelDaoImplTest extends AbstractTestNGSpringContextTests {
         m1.setPrice(BigDecimal.ZERO);
         m1.setCategory(c1);
         m1.setPieces(new ArrayList<Piece>());
-        e.persist(m1);
+        modelDao.create(m1);
         
-       Model m2 = new Model();
+        m2 = new Model();
         m2.setName("Model-2");
         m2.setAgeLimit(Byte.valueOf("25"));
         m2.setPrice(BigDecimal.ONE);
         m2.setCategory(c1);
         m2.setPieces(new ArrayList<Piece>());
-        e.persist(m2);
+        modelDao.create(m2);
+        
+        m3 = new Model();
+        m3.setName("Model-3");
+        m3.setAgeLimit(Byte.valueOf("60"));
+        m3.setPrice(new BigDecimal("500.00"));
+        m3.setCategory(c1);
+        m3.setPieces(new ArrayList<Piece>());
+        modelDao.create(m3);
         
         
         
@@ -76,50 +88,124 @@ public class ModelDaoImplTest extends AbstractTestNGSpringContextTests {
         //e.close();
     }
    
-    @AfterMethod
-    public void after() {
-        e.getTransaction().rollback();
-        e.close();
-    }
+    
 
-    @Test
-    public void testCreate() {
-        Model m3 = new Model();
-        m3.setName("Model-2");
-        m3.setAgeLimit(Byte.valueOf("25"));
-        m3.setPrice(BigDecimal.ONE);
-        m3.setCategory(c1);
-        m3.setPieces(new ArrayList<Piece>());
+        @Test(expectedExceptions=ConstraintViolationException.class)
+    public void testCreateWithNullName() {
+        Model m = new Model();
+   
+        m.setAgeLimit(Byte.valueOf("25"));
+        m.setPrice(BigDecimal.ONE);
+        m.setCategory(c1);
+        m.setPieces(new ArrayList<Piece>());
         
-       modelDao.create(m3);
-        
-        Assert.assertNotNull(m3.getId());
-        
-        
+       modelDao.create(m);
     }
+    
 
+ 
+    
     @Test
     public void testFindById() {
         Model res = modelDao.findById(m1.getId());
-        Assert.assertEquals(m1.getName(), res.getName());
+        Assert.assertEquals(res, m1);
     }
-
+    
+    @Test
+    public void testFindByNonExistingId() {
+        Model res = modelDao.findById(Long.MAX_VALUE);
+        Assert.assertNull(res);
+    }
+    
     @Test
     public void testFindByName() {
-        Model res = modelDao.findByName("Model-1");
-        Assert.assertEquals(m1.getId(), res.getId());
+        Model res = modelDao.findByName(m1.getName());
+        Assert.assertEquals(res, m1);
+    }
+    
+        @Test
+    public void testFindByNonExistingName() {
+        Model res = modelDao.findByName("Non existing");
+        Assert.assertNull(res);
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testFindByNullName() {
+        Model res = modelDao.findByName(null);
+    }
+    
     @Test
     public void testFindAll() {
         List<Model> list = modelDao.findAll();
-        Assert.assertEquals(2, list.size());
+        Assert.assertEquals(list.size(),3);
     }
 
     @Test
-    public void testDelete() {
-      modelDao.delete(m1);
-      Assert.assertNull(e.find(Model.class, m1.getId()));
+    public void testUpdate() {
+        String newName = "New Model";
+        m1.setName(newName);
+        modelDao.update(m1);
         
+        Model res = modelDao.findById(m1.getId());
+        Assert.assertEquals(res.getName(), newName);
     }
+    
+    @Test
+    public void testDelete() {
+      modelDao.delete(m2);
+      
+        Assert.assertEquals(modelDao.findAll().size(), 2);
+      Assert.assertNull(modelDao.findById(m2.getId()));
+    }
+    
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testDeleteNonExisting() {
+        Model m = new Model();
+        m.setName("Model_nonExisting");
+        m.setAgeLimit(Byte.valueOf("25"));
+        m.setPrice(new BigDecimal("8888.00"));
+        m.setCategory(c1);
+        m.setPieces(new ArrayList<Piece>());
+        
+        modelDao.delete(m);
+    }
+    
+    @Test(expectedExceptions = PersistenceException.class)
+    public void testNameIsUnique(){
+        Model m = new Model();
+        m.setName("Model-1");
+        m.setAgeLimit(Byte.valueOf("25"));
+        m.setPrice(BigDecimal.ONE);
+        m.setCategory(c1);
+        m.setPieces(new ArrayList<Piece>());
+        
+       modelDao.create(m);
+    }
+    
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testNegativePrice() {
+        Model m = new Model();
+        m.setName("Model-1");
+        m.setAgeLimit(Byte.valueOf("25"));
+        m.setPrice(new BigDecimal("-25.0"));
+        m.setCategory(c1);
+        m.setPieces(new ArrayList<Piece>());
+        
+       modelDao.create(m);
+    }
+    
+    @Test(expectedExceptions = ConstraintViolationException.class)
+    public void testNullPieces() {
+        Model m = new Model();
+        m.setName("Model-1");
+        m.setAgeLimit(Byte.valueOf("25"));
+        m.setPrice(new BigDecimal("25.0"));
+        m.setCategory(c1);
+        m.setPieces(null);
+        
+       modelDao.create(m);
+    }
+    
+    
+    
 }
