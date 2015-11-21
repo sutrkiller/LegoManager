d87fe219fe8924faf97155d1eb9f1687d28df99d
Laliska <sona.mastrakova@gmail.com>
2015-11-21 18:35:20 +0100

    added javadoc and missing methods to CategoryFacade implementation class

--- lego-service/src/main/java/cz/muni/fi/pa165/lego/service/facade/CategoryFacadeImpl.java
+++ lego-service/src/main/java/cz/muni/fi/pa165/lego/service/facade/CategoryFacadeImpl.java
@@ -4,18 +4,35 @@ import cz.muni.fi.pa165.lego.dto.CategoryCreateDTO;
 import cz.muni.fi.pa165.lego.dto.CategoryDTO;
 import cz.muni.fi.pa165.lego.facade.CategoryFacade;
 import java.util.List;
+
+import cz.muni.fi.pa165.lego.service.CategoryService;
+import cz.muni.fi.pa165.lego.service.MappingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
+ * CategoryFacadeImpl implements {@link CategoryFacade}.
  *
  * @author Sona Mastrakova <sona.mastrakova@gmail.com>
- * @date 11.11.2015
+ * @date 21.11.2015
  */
 @Service
+@Transactional
 public class CategoryFacadeImpl implements CategoryFacade {
 
     @Override
     public Long createCategory(CategoryCreateDTO categoryCreateDTO) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Long updateCategory(CategoryDTO categoryDTO) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Long deleteCategory(CategoryDTO categoryDTO) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
