c6c44d5fd2bb71d6fccdd0beb13376b4b171bb2f
Laliska <sona.mastrakova@gmail.com>
2015-11-21 21:06:58 +0100

    PieceService and PieceTypeService implementation completed

--- lego-service/src/main/java/cz/muni/fi/pa165/lego/service/PieceServiceImpl.java
+++ lego-service/src/main/java/cz/muni/fi/pa165/lego/service/PieceServiceImpl.java
@@ -1,51 +1,64 @@
-
 package cz.muni.fi.pa165.lego.service;
 
+import cz.muni.fi.pa165.lego.service.exceptions.LegoServiceException;
 import cz.muni.fi.pa165.legomanager.dao.PieceDao;
 import cz.muni.fi.pa165.legomanager.entities.Piece;
+import cz.muni.fi.pa165.legomanager.exceptions.LegoPersistenceException;
 import java.util.List;
 import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 
 /**
+ * Implementation of the {@link PieceService}.
  *
  * @author Sona Mastrakova <sona.mastrakova@gmail.com>
+ * @date 21.11.2015
  */
-
 @Service
 public class PieceServiceImpl implements PieceService {
-    
+
     @Inject
     private PieceDao pieceDao;
 
     @Override
     public void create(Piece piece) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            pieceDao.create(piece);
+        } catch (LegoPersistenceException ex) {
+            throw new LegoServiceException("Creating Piece entity " + piece + " failed.", ex);
+        }
     }
 
     @Override
     public void update(Piece piece) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            pieceDao.update(piece);
+        } catch (LegoPersistenceException ex) {
+            throw new LegoServiceException("Updating Piece entity " + piece + " failed.", ex);
+        }
     }
 
     @Override
     public void delete(Piece piece) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            pieceDao.delete(piece);
+        } catch (LegoPersistenceException ex) {
+            throw new LegoServiceException("Deleting Piece entity " + piece + " failed.", ex);
+        }
     }
 
     @Override
     public Piece findById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public Piece findByName(String name) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            return pieceDao.findById(id);
+        } catch (LegoPersistenceException ex) {
+            throw new LegoServiceException("Looking for Piece entity with id " + id + " failed.", ex);
+        }
     }
 
     @Override
     public List<Piece> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return pieceDao.findAll();
     }
-    
+
 }
