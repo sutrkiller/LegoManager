3c12acc308284c28f5229aa6bf28dc430bc58a96
Laliska <sona.mastrakova@gmail.com>
2015-11-21 18:34:35 +0100

    added javadoc to CategoryFacade interface

--- lego-api/src/main/java/cz/muni/fi/pa165/lego/facade/CategoryFacade.java
+++ lego-api/src/main/java/cz/muni/fi/pa165/lego/facade/CategoryFacade.java
@@ -5,17 +5,50 @@ import cz.muni.fi.pa165.lego.dto.CategoryDTO;
 import java.util.List;
 
 /**
+ * CategoryFacade defines operations available within the Lego API to simplify the usage of the application.
  *
  * @author Sona Mastrakova <sona.mastrakova@gmail.com>
  * @date 11.11.2015
  */
 public interface CategoryFacade {
 
+    /**
+     * Create the given category.
+     *
+     * @param categoryCreateDTO category to be created
+     */
     Long createCategory(CategoryCreateDTO categoryCreateDTO);
 
+    /**
+     * Update the given category.
+     *
+     * @param categoryDTO category to be updated
+     */
+    Long updateCategory(CategoryDTO categoryDTO);
+
+    /**
+     * Delete the given category.
+     *
+     * @param categoryDTO category to be deleted
+     */
+    Long deleteCategory(CategoryDTO categoryDTO);
+
+    /**
+     * Get category with the given ID.
+     *
+     * @param id id of the category
+     */
     CategoryDTO getCategoryById(Long id);
 
+    /**
+     * Get category with the given name.
+     *
+     * @param name name of the category
+     */
     CategoryDTO getCategoryByName(String name);
 
+    /**
+     * Get all existing categories.
+     */
     List<CategoryDTO> getAllCategories();
 }
