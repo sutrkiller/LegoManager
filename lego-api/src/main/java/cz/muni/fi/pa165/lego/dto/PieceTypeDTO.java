3a9e20adf7d588ef89554dc2060bab5f6ca50ac9
sutrkiller <tobias.kamenicky@gmail.com>
2015-11-25 22:52:30 +0100

    PieceTypeDTO completed

--- lego-api/src/main/java/cz/muni/fi/pa165/lego/dto/PieceTypeDTO.java
+++ lego-api/src/main/java/cz/muni/fi/pa165/lego/dto/PieceTypeDTO.java
@@ -1,13 +1,29 @@
 package cz.muni.fi.pa165.lego.dto;
 
+
+import cz.muni.fi.pa165.lego.enums.Color;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
- * TODO complete
- * @author Ondrej Velisek <ondrejvelisek@gmail.com>
+ * 
+ * @author Tobias Kamenicky <tobias.kamenicky@gmail.com>
+ * @date 25.11.2015
  */
 public class PieceTypeDTO {
 
     private Long id;
 
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String name;
+    
+    @NotNull
+    private Set<Color> colors = new HashSet<>();
+    
     public Long getId() {
         return id;
     }
@@ -15,4 +31,55 @@ public class PieceTypeDTO {
     public void setId(Long id) {
         this.id = id;
     }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Color> getColors() {
+        return colors;
+    }
+
+    public void setColors(Set<Color> colors) {
+        this.colors = colors;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof PieceTypeDTO)) {
+            return false;
+        }
+        final PieceTypeDTO other = (PieceTypeDTO) obj;
+        if (this.getId() == null) {
+            return false;
+        }
+        if (!Objects.equals(this.getId(), other.getId())) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.getId());
+    }
+
+    @Override
+    public String toString() {
+        return "PieceTypeDTO{" + "id=" + id + ", name=" + name + ", colors=" + colors + '}';
+    }
+    
+    
+    
+    
 }
