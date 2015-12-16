14085f67ed62dfa2e1a6da91a276e89b8a7150cb
Laliska <sona.mastrakova@gmail.com>
2015-12-16 01:43:43 +0100

    store page and home page immproved

---
+++ lego-spring-mvc/src/main/java/cz/muni/fi/pa165/lego/mvc/controllers/StoreController.java
@@ -0,0 +1,112 @@
+package cz.muni.fi.pa165.lego.mvc.controllers;
+
+import cz.muni.fi.pa165.lego.dto.CategoryDTO;
+import cz.muni.fi.pa165.lego.dto.LegoSetDTO;
+import cz.muni.fi.pa165.lego.dto.ModelDTO;
+import cz.muni.fi.pa165.lego.dto.PieceTypeDTO;
+import cz.muni.fi.pa165.lego.facade.CategoryFacade;
+import cz.muni.fi.pa165.lego.facade.LegoSetFacade;
+import cz.muni.fi.pa165.lego.facade.ModelFacade;
+import cz.muni.fi.pa165.lego.facade.PieceTypeFacade;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Sona Mastrakova <sona.mastrakova@gmail.com>
+ * @date 15.12.2015
+ */
+@Controller
+@RequestMapping("/store")
+public class StoreController {
+
+    final static Logger log = LoggerFactory.getLogger(StoreController.class);
+
+    @Inject
+    private CategoryFacade categoryFacade;
+
+    @Inject
+    private PieceTypeFacade pieceTypeFacade;
+
+    @Inject
+    private ModelFacade modelFacade;
+
+    @Inject
+    private LegoSetFacade legoSetFacade;
+
+    public void setCategoryFacade(CategoryFacade categoryFacade) {
+        this.categoryFacade = categoryFacade;
+    }
+
+    public void setPieceTypeFacade(PieceTypeFacade pieceTypeFacade) {
+        this.pieceTypeFacade = pieceTypeFacade;
+    }
+
+    public void setModelFacade(ModelFacade modelFacade) {
+        this.modelFacade = modelFacade;
+    }
+
+    public void setLegoSetFacade(LegoSetFacade legoSetFacade) {
+        this.legoSetFacade = legoSetFacade;
+    }
+
+    /**
+     * Shows all categories and products.
+     *
+     * @param model data to display
+     * @return JSP page name
+     */
+    @RequestMapping("/show")
+    public String list(Model model) {
+        log.debug("show()");
+
+        List<CategoryDTO> allCategories = categoryFacade.findAll();
+        List<PieceTypeDTO> allPieceTypes = pieceTypeFacade.findAll();
+        List<ModelDTO> allModels = modelFacade.findAll();
+        //List<LegoSetDTO> allLegoSets = legoSetFacade.findAll();
+
+        model.addAttribute("categories", allCategories);
+        model.addAttribute("piecetypes", allPieceTypes);
+        model.addAttribute("models", allModels);
+        //model.addAttribute("legosets", allLegoSets);
+
+        return "store/show";
+    }
+
+    /**
+     * Shows product detail.
+     *
+     * @param id product id
+     * @param model data to display
+     * @return JSP page name
+     */
+    /*@RequestMapping("/product/{id}")
+    public String product(@PathVariable long id, Model model) {
+        log.debug("product({})", id);
+        model.addAttribute("product", productFacade.getProductWithId(id));
+        return "store/product";
+    }*/
+
+    /**
+     * Shows category detail.
+     *
+     * @param id category id
+     * @param model data to display
+     * @return JSP page name
+     */
+    /*@RequestMapping("/category/{id}")
+    public String category(@PathVariable long id, Model model) {
+        log.debug("category({})", id);
+        CategoryDTO categoryDTO = categoryFacade.findById(id);
+        model.addAttribute("category", categoryDTO);
+        return "store/category";
+    }*/
+}
--- lego-spring-mvc/src/main/webapp/WEB-INF/jsp/home.jsp
+++ lego-spring-mvc/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -1,9 +1,23 @@
 <%@ page contentType="text/html;charset=UTF-8" pageEncoding="utf-8" trimDirectiveWhitespaces="true" session="false" %>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    </head>
-    <body>
-        <h1>Welcome to Spring MVC!</h1>
-    </body>
-</html>
+<%@ taglib tagdir="/WEB-INF/tags" prefix="my" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
+<my:pagetemplate>
+    <jsp:attribute name="body">
+
+        <div class="jumbotron">
+            <h1>Welcome to Lego Manager!</h1>
+            <p class="lead">Lego Manager is a Java application, that serves to easily manage a Lego storage of your shop. </p>
+            <p class="lead"><small>This Java application is created as a semestral project within a course PA165 Enterprise Java applications at MU.</small></p>
+            <p><a class="btn btn-lg btn-info" href="${pageContext.request.contextPath}/store/show"
+                  role="button">I'm a customer!</a>
+                <a class="btn btn-lg btn-warning" href="${pageContext.request.contextPath}/login"
+                   role="button">I'm a Lego admin!</a></p>
+        </div>
+
+    </jsp:attribute>
+</my:pagetemplate>
